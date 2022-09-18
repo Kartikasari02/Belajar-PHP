01648c9cb47587ae211966dc48e1d686dcecdfd1
Kartika Sari <110078803+Kartikasari02@users.noreply.github.com>
2022-09-18 22:25:58 +0700

    .input datadiri (16/09/22)

--- input-datadiri.php
+++ input-datadiri.php
@@ -1,20 +1,6 @@
-<form action="input-datadiri.php" method="POST">
-    <label for="nis">Nomor Induk Siswa :</label>
-    <input type="number" name="nis" placeholder="Ex. 12003102" /><br>
-
-    <label for="nama">Nama Lengkap :</label>
-    <input type="text" name="nama" placholder="Ex. Firdaus" /><br>
-
-    <label for="tanggal_lahir">Tanggal Lahir :</label>
-    <input type="date" name="tanggal_lahir" /><br>
-    
-    <label for="nilai">Nilai :</label>
-    <input type="number" name="nilai" placeholder="Ex. 80.56"/><br>
-
-    <input type="submit" name="simpan" value="Simpan Data"/>
-
-    <?php 
+<?php 
         include('./input-config.php');
+        echo "<a href='input-datadiri-tambah.php'>Tambah Data</a>";
         echo "<hr>";
         //Menampilkan data dari databases
         $no= 1;
@@ -27,6 +13,12 @@
                     <td>".$row["namalengkap"]."</td>
                     <td>".$row["tanggal_lahir"]."</td>
                     <td>".$row["nilai"]."</td>
+                    <td>
+                        <a href='input-datadiri-edit.php?nis=".$row["nis"]."'>Edit</a>
+                        &nbsp;-&nbsp;
+                        <a href='input-datadiri-hapus.php?nis=".$row["nis"]."'
+                        onclick='return confirm(\"Yakin Hapus?\");'>Hapus</a>
+                    <td>
                 </tr>
             ";
             $no++;
@@ -39,6 +31,7 @@
                     <th>Nama Lengkap</th>
                     <th>Tanggal_lahir</th>
                     <th>Nilai</th>
+                    <th>Aksi</th>
                 </tr>
                 $tabeldata
             </tabel>
